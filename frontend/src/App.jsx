2b0c67e6a49b156f6750fe79b449b8106a4a8422
rashmi-ranjan-sahoo-dev <rinkuasahoo04@gmail.com>
2025-12-08 01:15:01 +0530

    complete the ai frontend part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import "./App.css"
+import "prismjs/themes/prism-tomorrow.css"
+import Editer from "react-simple-code-editor"
+import prism from "prismjs"
+import Markdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css"
+import axios from "axios"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const [code, setCode] = useState(` function sum(){
+    return 1+1
+    }`)
+
+    const [data, setData] = useState("")
+
+  useEffect(() =>{
+    prism.highlightAll()
+  },[])
+
+  async function  reviewCode() {
+    
+    try{
+        const response = await axios.post("http://localhost:3000/ai/get-review",
+        {
+          code: code
+        })
+
+      // console.log(response.data)
+
+      setData(response.data)
+    }catch(error){
+      console.error(error);
+    }
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+         <div className='left'>
+          <div className='code'>
+           <Editer
+           value={code}
+           onValueChange={code => setCode(code)}
+           highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+           padding={10}
+           style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 20,
+            borderRadius: "5px",
+            height:"100%",
+            width: "100",
+            color:"white"
+           }}
+           />
+          </div>
+          <div
+          onClick={reviewCode}
+           className="review">Review</div>
+         </div>
+         <div
+           style={{
+            color:"white"
+           }}
+         className="right">
+          <Markdown 
+          rehypePlugins={[ rehypeHighlight]}>{data}</Markdown></div>
+    </main>
   )
 }
 
